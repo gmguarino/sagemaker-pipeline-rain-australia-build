c6239162b4b1c0803fd020bf960e4304c413f9b4
Giuseppe Guarino <gmguarino1@gmail.com>
2022-01-18 14:10:53 +0100

    changed logging

--- pipelines/rain/pipeline.py
+++ pipelines/rain/pipeline.py
@@ -55,8 +55,9 @@ BASE_DIR = os.path.dirname(os.path.realpath(__file__))
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
-logger.addHandler(logging.StreamHandler(sys.stdout))
-
+logger.addHandler(logging.StreamHandler(sys.stloggerdout))
+logger.debug("Entered script")
+logger.info("Entered script")
 
 def get_session(region, default_bucket):
     """Gets the sagemaker session based on the region.
@@ -68,11 +69,14 @@ def get_session(region, default_bucket):
     Returns:
         `sagemaker.session.Session instance
     """
+    logger.debug("Getting session")
 
     boto_session = boto3.Session(region_name=region)
 
     sagemaker_client = boto_session.client("sagemaker")
     runtime_client = boto_session.client("sagemaker-runtime")
+    logger.info("Session Obtained")
+    
     return sagemaker.session.Session(
         boto_session=boto_session,
         sagemaker_client=sagemaker_client,
@@ -81,74 +85,74 @@ def get_session(region, default_bucket):
     )
 
 
-def resolve_ecr_uri_from_image_versions(sagemaker_session, image_versions, image_name):
-    """ Gets ECR URI from image versions
-    Args:
-        sagemaker_session: boto3 session for sagemaker client
-        image_versions: list of the image versions
-        image_name: Name of the image
+# def resolve_ecr_uri_from_image_versions(sagemaker_session, image_versions, image_name):
+#     """ Gets ECR URI from image versions
+#     Args:
+#         sagemaker_session: boto3 session for sagemaker client
+#         image_versions: list of the image versions
+#         image_name: Name of the image
 
-    Returns:
-        ECR URI of the image version
-    """
+#     Returns:
+#         ECR URI of the image version
+#     """
 
-    # Fetch image details to get the Base Image URI
-    for image_version in image_versions:
-        if image_version['ImageVersionStatus'] == 'CREATED':
-            image_arn = image_version["ImageVersionArn"]
-            version = image_version["Version"]
-            logger.info(f"Identified the latest image version: {image_arn}")
-            response = sagemaker_session.sagemaker_client.describe_image_version(
-                ImageName=image_name,
-                Version=version
-            )
-            return response['ContainerImage']
-    return None
+#     # Fetch image details to get the Base Image URI
+#     for image_version in image_versions:
+#         if image_version['ImageVersionStatus'] == 'CREATED':
+#             image_arn = image_version["ImageVersionArn"]
+#             version = image_version["Version"]
+#             logger.info(f"Identified the latest image version: {image_arn}")
+#             response = sagemaker_session.sagemaker_client.describe_image_version(
+#                 ImageName=image_name,
+#                 Version=version
+#             )
+#             return response['ContainerImage']
+#     return None
 
 
-def resolve_ecr_uri(sagemaker_session, image_arn):
-    """Gets the ECR URI from the image name
+# def resolve_ecr_uri(sagemaker_session, image_arn):
+#     """Gets the ECR URI from the image name
 
-    Args:
-        sagemaker_session: boto3 session for sagemaker client
-        image_name: name of the image
+#     Args:
+#         sagemaker_session: boto3 session for sagemaker client
+#         image_name: name of the image
 
-    Returns:
-        ECR URI of the latest image version
-    """
+#     Returns:
+#         ECR URI of the latest image version
+#     """
 
-    # Fetching image name from image_arn (^arn:aws(-[\w]+)*:sagemaker:.+:[0-9]{12}:image/[a-z0-9]([-.]?[a-z0-9])*$)
-    image_name = image_arn.partition("image/")[2]
-    try:
-        # Fetch the image versions
-        next_token = ''
-        while True:
-            response = sagemaker_session.sagemaker_client.list_image_versions(
-                ImageName=image_name,
-                MaxResults=100,
-                SortBy='VERSION',
-                SortOrder='DESCENDING',
-                NextToken=next_token
-            )
-            ecr_uri = resolve_ecr_uri_from_image_versions(
-                sagemaker_session, response['ImageVersions'], image_name)
-            if "NextToken" in response:
-                next_token = response["NextToken"]
+#     # Fetching image name from image_arn (^arn:aws(-[\w]+)*:sagemaker:.+:[0-9]{12}:image/[a-z0-9]([-.]?[a-z0-9])*$)
+#     image_name = image_arn.partition("image/")[2]
+#     try:
+#         # Fetch the image versions
+#         next_token = ''
+#         while True:
+#             response = sagemaker_session.sagemaker_client.list_image_versions(
+#                 ImageName=image_name,
+#                 MaxResults=100,
+#                 SortBy='VERSION',
+#                 SortOrder='DESCENDING',
+#                 NextToken=next_token
+#             )
+#             ecr_uri = resolve_ecr_uri_from_image_versions(
+#                 sagemaker_session, response['ImageVersions'], image_name)
+#             if "NextToken" in response:
+#                 next_token = response["NextToken"]
 
-            if ecr_uri is not None:
-                return ecr_uri
+#             if ecr_uri is not None:
+#                 return ecr_uri
 
-        # Return error if no versions of the image found
-        error_message = (
-            f"No image version found for image name: {image_name}"
-        )
-        logger.error(error_message)
-        raise Exception(error_message)
+#         # Return error if no versions of the image found
+#         error_message = (
+#             f"No image version found for image name: {image_name}"
+#         )
+#         logger.error(error_message)
+#         raise Exception(error_message)
 
-    except (ClientError, sagemaker_session.sagemaker_client.exceptions.ResourceNotFound) as e:
-        error_message = e.response["Error"]["Message"]
-        logger.error(error_message)
-        raise Exception(error_message)
+#     except (ClientError, sagemaker_session.sagemaker_client.exceptions.ResourceNotFound) as e:
+#         error_message = e.response["Error"]["Message"]
+#         logger.error(error_message)
+#         raise Exception(error_message)
 
 
 def get_pipeline(
@@ -173,6 +177,8 @@ def get_pipeline(
     sagemaker_session = get_session(region, default_bucket)
     if role is None:
         role = sagemaker.session.get_execution_role(sagemaker_session)
+
+    logger.debug("Define parameters")
 
     # parameters for pipeline execution
     processing_instance_count = ParameterInteger(
