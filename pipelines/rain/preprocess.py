96056539d6f3887563c32a9f5cfebb56bd4eff07
Giuseppe Guarino <gmguarino1@gmail.com>
2022-01-24 15:28:37 +0100

    added scaler model fns

--- pipelines/rain/preprocess.py
+++ pipelines/rain/preprocess.py
@@ -167,3 +167,70 @@ if __name__ == "__main__":
     with tarfile.open(f"{base_dir}/preprocess/model.tar.gz", "w:gz") as tf:
         tf.add(f"model.joblib")
 
+def input_fn(input_data, content_type):
+    """Parse input data payload
+
+    We currently only take csv input. Since we need to process both labelled
+    and unlabelled data we first determine whether the label column is present
+    by looking at how many columns were provided.
+    """
+    if content_type == "text/csv":
+        # Read the raw input data as CSV.
+        df = pd.read_csv(StringIO(input_data), header=None)
+
+        if len(df.columns) == len(feature_columns) + 1:
+            # This is a labelled example, includes the ring label
+            df.columns = feature_columns + [label_column]
+        elif len(df.columns) == len(feature_columns):
+            # This is an unlabelled example.
+            df.columns = feature_columns
+
+        return df
+    else:
+        raise ValueError("{} not supported by script!".format(content_type))
+
+
+def output_fn(prediction, accept):
+    """Format prediction output
+
+    The default accept/content-type between containers for serial inference is JSON.
+    We also want to set the ContentType or mimetype as the same value as accept so the next
+    container can read the response payload correctly.
+    """
+    if accept == "application/json":
+        instances = []
+        for row in prediction.tolist():
+            instances.append(row)
+        json_output = {"instances": instances}
+
+        return worker.Response(json.dumps(json_output), mimetype=accept)
+    elif accept == "text/csv":
+        return worker.Response(encoders.encode(prediction, accept), mimetype=accept)
+    else:
+        raise RuntimeException("{} accept type is not supported by this script.".format(accept))
+
+
+def predict_fn(input_data, model):
+    """Preprocess input data
+
+    We implement this because the default predict_fn uses .predict(), but our model is a preprocessor
+    so we want to use .transform().
+
+    The output is returned in the following order:
+
+        rest of features either one hot encoded or standardized
+    """
+    features = model.transform(input_data)
+
+    if label_column in input_data:
+        # Return the label (as the first column) and the set of features.
+        return np.insert(features, 0, input_data[label_column], axis=1)
+    else:
+        # Return only the set of features
+        return features
+
+
+def model_fn(model_dir):
+    """Deserialize fitted model"""
+    preprocessor = joblib.load(os.path.join(model_dir, "model.joblib"))
+    return preprocessor
