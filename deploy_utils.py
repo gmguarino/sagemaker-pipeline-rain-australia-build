241a27fa4efca735d0dd9b48c768fe4a5861a788
Giuseppe Guarino <gmguarino1@gmail.com>
2022-01-24 18:50:59 +0100

    added deployment script

---
+++ deploy_utils.py
@@ -0,0 +1,60 @@
+# Python script for deployment used with the notebook pipeline development!
+# In production there is a separate repository that builds the code
+
+
+from asyncio.log import logger
+from multiprocessing.connection import Client
+import boto3
+import argparse
+import logging
+import os
+import zipfile
+import tarfile
+
+from botocore.exceptions import ClientError
+
+logger = logging.getLogger(__name__)
+sm_client = boto3.client("sagemaker")
+
+def get_latest_approved_package(model_package_group_name):
+
+    try: 
+        response = sm_client.list_model_packages(
+            ModelPackageGroupName=model_package_group_name,
+            ModelApprovalStatus="Approved",
+            SortBy="CreationTime",
+            MaxResults=100,
+        )
+        approved_packages = response["ModelPackageSummaryList"]
+
+        # Fetch more packages if none returned with continuation token
+        while len(approved_packages) == 0 and "NextToken" in response:
+            logger.debug("Getting more packages for token: {}".format(response["NextToken"]))
+            response = sm_client.list_model_packages(
+                ModelPackageGroupName=model_package_group_name,
+                ModelApprovalStatus="Approved",
+                SortBy="CreationTime",
+                MaxResults=100,
+                NextToken=response["NextToken"],
+            )
+            approved_packages.extend(response["ModelPackageSummaryList"])
+        
+        if len(approved_packages) == 0:
+            # raise err if no packages found
+            error_message = (
+                f"No approved Model Package found for ModelPackageGroup: {model_package_group_name}"
+            )
+            logger.error(error_message)
+            raise Exception(error_message)
+        
+        # otherwise get latest model arn
+        model_package_arn = approved_packages[0]["ModelPackageArn"]
+        logger.info(f"Identified the latest approved model package: {model_package_arn}")
+        return approved_packages[0]
+    
+    except ClientError as err:
+        error_message = err.response["Error"]["Message"]
+        logger.error(error_message)
+        raise Exception(error_message)
+
+
